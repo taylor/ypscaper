ab5116d3084172a23e25a88b4cc6108ac6cc5674
Taylor Carpenter <taylor@codecafe.com>
2009-04-02 03:10:04 -0500

    all working specs except get_page

--- ypscraper_spec.rb
+++ ypscraper_spec.rb
@@ -16,35 +16,45 @@ describe YPScraper do
     yp.default_provider.should_not == nil
   end
 
-  it "should have a provider switchboad set if switchboard is specified when creating a new YP object" do
-    @yp.default_provider.should == :switchboard
+  it "should have a provider yellowpages set if yellowpages is specified when creating a new YP object" do
+    yp = YPScraper.new(:yellowpages)
+    yp.current_provider.should == :yellowpages
   end
 
-  it "should have a valid url for provider switchboard" do
-    @yp.provider[:switchboard][:uri].should == 'http://www.switchboard.com'
+  it "should be able to access the uri for the default provider via the provider method and receive a valid response" do
+    @yp.provider.uri.should == "http://www.switchboard.com"
+  end
+
+  it "should be able to access the uri for superpages when super pages is passed to the provider method" do
+    @yp.provider(:superpages).uri.should == 'http://yellowpages.superpages.com'
+  end
+
+  it "should be able to add new providers" do
+    @yp.add_provider(:foo, 'http://foo.com', 'sp', 'sk', 'location')
+    @yp.provider(:foo).uri.should == 'http://foo.com'
   end
 
   it "should have a default path set for provider switchboard" do
-    @yp.provider[:switchboard][:search_path].should == "results.htm"
+    @yp.provider(:switchboard).search_path.should == "results.htm"
   end
 
   it "should have a keyword identifier of 'KW' for provider switchboard" do
-    @yp.provider[:switchboard][:sk].should == "KW"
+    @yp.provider(:switchboard).sk.should == "KW"
   end
 
 
   it "should have a location identifier of 'LO' for provider switchboard" do
-    @yp.provider[:switchboard][:lk].should == "LO"
+    @yp.provider(:switchboard).lk.should == "LO"
   end
 
-  it "should be able to get a page using mechanize if a url is given for get_page" do
-    @yp.get_page(@yp.provider[@yp.default_provider][:uri]).title.should == 'foo'
+  it "should be able to get a url and show the title for that page" do
+    @yp.get_page(@yp.provider(:switchboard).uri).title.should == 'Yellow Pages, White Pages, Maps, and more - Switchboard.com'
   end
 
-  it "should have a valid title set when for provider switchboard" do
-    @yp = YPScraper.new(:switchboard)
-    @yp.search("dentist", "austin, tx").title.should == "dentist in Austin, TX - Yellow Pages - Switchboard.com"
-  end
+  # it "should have a valid title set when for provider switchboard" do
+  #   @yp = YPScraper.new(:switchboard)
+  #   @yp.search("dentist", "austin, tx").title.should == "dentist in Austin, TX - Yellow Pages - Switchboard.com"
+  # end
 
-    #@yp.provider[:switchboard][:search_path].should == "results.htm?KW=dentist&LO=austin%2C+tx"
+  #@yp.provider[:switchboard][:search_path].should == "results.htm?KW=dentist&LO=austin%2C+tx"
 end
